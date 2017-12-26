60d16716589c7d51e542469253cf8ae5728c2a06
andy-bell <andy.bell.github@gmail.com>
2017-12-26 17:01:02 +0000

    remove take(2)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
     let pool = ThreadPool::new(4);
 
-    for stream in listener.incoming().take(2) {
+    for stream in listener.incoming() {
         let stream = stream.unwrap();
 
         pool.execute(|| {
