784506df7a5bc3ab4fa17982caf96d0d78fc5de0
andy-bell <andy.bell.github@gmail.com>
2018-01-06 22:54:34 +0000

    added basic router function porting code from main to lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,7 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::fs::File;
+
 pub mod thread_pool;
 pub mod worker;
 
@@ -16,4 +20,24 @@ type Job = Box<FnBox + Send + 'static>;
 pub enum Message {
     NewJob(Job),
     Terminate,
+}
+
+pub fn router (mut stream: &TcpStream) -> (std::string::String, std::string::String) {
+    let mut buffer = [0; 512];
+    stream.read(&mut buffer).unwrap();
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let (status_line, filename) = if buffer.starts_with(get) {
+        (String::from("HTTP/1.1 200 OK\r\n\r\n"), "views/hello.html")
+    } else {
+        (String::from("HTTP/1.1 404 NOT FOUND\r\n\r\n"), "views/404.html")
+    };
+
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+
+    file.read_to_string(&mut contents).unwrap();
+
+    return (status_line, contents)
+    
 }
