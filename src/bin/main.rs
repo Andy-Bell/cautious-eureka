fa8acdc5de2e7392625cd1a4900347170831b674
andy-bell <andy.bell.github@gmail.com>
2018-01-06 22:54:34 +0000

    added basic router function porting code from main to lib

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -3,7 +3,6 @@ use cautious_eureka::thread_pool::ThreadPool;
 use std::io::prelude::*;
 use std::net::TcpListener;
 use std::net::TcpStream;
-use std::fs::File;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -22,21 +21,8 @@ fn main() {
 
 
 fn handle_connection(mut stream: TcpStream) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK\r\n\r\n", "views/hello.html")
-    } else {
-        ("HTTP/1.1 404 NOT FOUND\r\n\r\n", "views/404.html")
-    };
-
-    let mut file = File::open(filename).unwrap();
-    let mut contents = String::new();
-
-    file.read_to_string(&mut contents).unwrap();
-
+    let (status_line, contents) = cautious_eureka::router(&stream);
     let response = format!("{}{}", status_line, contents);
 
     stream.write(response.as_bytes()).unwrap();
