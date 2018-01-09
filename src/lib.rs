50d92c56a377562baeaf31c22c56f60c5c01b349
andy-bell <andy.bell.github@gmail.com>
2018-01-09 12:56:23 +0000

    Router now matches elements of buffer instead of direct string

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::io::prelude::*;
 use std::net::TcpStream;
 use std::fs::File;
+use std::str;
 
 pub mod thread_pool;
 pub mod worker;
@@ -26,14 +27,21 @@ pub fn router (mut stream: &TcpStream) -> (std::string::String, std::string::Str
     let mut buffer = [0; 512];
     stream.read(&mut buffer).unwrap();
 
-    let get = b"GET / HTTP/1.1\r\n";
-    let (status_line, filename) = if buffer.starts_with(get) {
-        (String::from("HTTP/1.1 200 OK\r\n\r\n"), "views/hello.html")
-    } else {
-        (String::from("HTTP/1.1 404 NOT FOUND\r\n\r\n"), "views/404.html")
+    let s = match str::from_utf8(&buffer) {
+        Ok(v) => v,
+        Err(e) => panic!("Invalid UTF-8 sequence: {}", e),
     };
 
-    let mut file = File::open(filename).unwrap();
+
+    let array: Vec<&str> = s.split(" ").collect();
+
+    let (status_line, filename) = if array[0] == "GET" && array[1] == "/" {
+        (String::from("HTTP/1.1 200 OK\r\n\r\n"), "hello.html")
+    } else {
+        (String::from("HTTP/1.1 404 NOT FOUND\r\n\r\n"), "404.html")
+    };
+
+    let mut file = File::open(format!("views/{}", filename)).unwrap();
     let mut contents = String::new();
 
     file.read_to_string(&mut contents).unwrap();
