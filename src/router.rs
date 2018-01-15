c6b0ccec44a36c11a29993a1580125f03fd84896
Andy Bell <andy.bell.github@gmail.com>
2018-01-15 13:42:08 +0000

    Fix issues with router cancelling connection early refactored stream parsing out of router back into main, will need this to be put into own module later, but did not make sense in the routing module for now.

--- src/router.rs
+++ src/router.rs
@@ -1,22 +1,16 @@
 extern crate httparse;
 
 use std::io::prelude::*;
-use std::net::TcpStream;
-use std::str;
 use std::fs::File;
 use std::string::String;
 
-pub fn router (mut stream: &TcpStream) -> (String, String) {
-    let mut buffer = [0; 512];
-    stream.read(&mut buffer).unwrap();
-
+pub fn router (buffer: &[u8]) -> (String, String) {
     let mut headers = [httparse::EMPTY_HEADER; 16];
     let mut req = httparse::Request::new(&mut headers);
 
-    let res = req.parse(&buffer).unwrap();
+    let _res = req.parse(buffer).unwrap();
 
-    let (status_line, filename) = if res.is_partial() {
-        match req.path {
+    let (status_line, filename) = match req.path {
             Some(ref path) => {
                 if path.to_string() == "/" {
                     (String::from("HTTP/1.1 200 OK\r\n\r\n"), "hello.html")
@@ -27,8 +21,8 @@ pub fn router (mut stream: &TcpStream) -> (String, String) {
             None => {
                 panic!("no req.path");
             }
-        }
-    };
+        };
+
 
     let mut file = File::open(format!("views/{}", filename)).unwrap();
     let mut contents = String::new();
